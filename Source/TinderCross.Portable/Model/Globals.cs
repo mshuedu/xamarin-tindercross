8dbc6a571846b8ff8662ef99fe1ca2a4c2219d79
unknown <balintf@balintf1.europe.corp.microsoft.com>
2015-03-21 11:17:07 +0100

    Added first complete version of app.

---
+++ Source/TinderCross.Portable/Model/Globals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TinderCross.Portable.Model
+{
+	public static class Globals
+	{
+		public static MainModel MainModel { get; set; }
+
+		static Globals ()
+		{
+			MainModel = new MainModel ();
+		}
+	}
+}
+
---
+++ Source/TinderCross.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using TinderCross.Portable.ViewModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace TinderCross.WindowsPhone
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MainViewModel mainViewModel;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            mainViewModel = new MainViewModel("Balint (Windows Phone)");
+            await mainViewModel.Initialize();
+
+            RefreshView();
+        }
+
+        private void RefreshView()
+        {
+            var currentGirl = mainViewModel.GetCurrentGirl();
+            if (currentGirl != null)
+            {
+                GirlImage.Source = new BitmapImage(new Uri(currentGirl.ImageUrl));
+                NameTextBlock.Text = currentGirl.Name;
+                AgeTextBlock.Text = currentGirl.Age.ToString();
+            }
+            else
+            {
+                GirlImage.Source = null;
+                NameTextBlock.Text = "Nincs több lány az adatbázisban";
+                AgeTextBlock.Text = "";
+                NopeButton.IsEnabled = false;
+                LikedButton.IsEnabled = false;
+            }
+        }
+
+        private async void NopeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentGirl = mainViewModel.GetCurrentGirl();
+            if (currentGirl != null)
+            {
+                await mainViewModel.AddLike(false);
+                mainViewModel.MoveToNextGirl();
+                RefreshView();
+            }
+        }
+
+        private async void LikedButton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentGirl = mainViewModel.GetCurrentGirl();
+            if (currentGirl != null)
+            {
+                await mainViewModel.AddLike(true);
+                mainViewModel.MoveToNextGirl();
+                RefreshView();
+            }
+        }
+    }
+}
